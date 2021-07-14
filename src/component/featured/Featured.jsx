afdf615b96e18920cd43783ba5519528311a50a9
bisaksss <bisaksss@gmail.com>
2021-07-14 12:45:19 +0530

    add catogory button to home page

--- src/component/featured/Featured.jsx
+++ src/component/featured/Featured.jsx
@@ -1,9 +1,26 @@
 import { InfoOutlined, PlayArrow } from "@material-ui/icons";
 import "./featured.scss";
 
-export default function Featured() {
+export default function Featured({ typez }) {
     return (
         <div className="featured">
+
+            {typez && (
+                <div className="catogory">
+                         <span>{typez === "cinemantic" ? "Cinemantic" : "Relax" }</span>
+            
+                        <select name="genre" id="genre">
+                            <option >Select</option>
+                            <option value="test1">test1</option>
+                            <option value="test2">test2</option>
+                            <option value="test3">test3</option>
+                            <option value="test4">test4</option>
+                            <option value="test5">test5</option>
+                        </select>
+
+                </div>
+           
+            )}
             <img src="assets/cover.jpg" alt="" />
 
             <div className="info">
