fc801967d8da65c6deb444a0513fb4f2cb60e02e
bisaksss <bisaksss@gmail.com>
2021-08-14 20:22:33 +0530

    add rolling to list

--- src/component/list/List.jsx
+++ src/component/list/List.jsx
@@ -1,14 +1,43 @@
 import { ArrowBackIos, ArrowForwardIos } from "@material-ui/icons";
+import { useState } from "react";
+import { useRef } from "react";
 import ListItem from "../listItem/ListItem";
 import "./list.scss";
 
 export default function List() {
+    const[isMoved,setIsMoved]=useState(false);
+    const [slideNumber , setSlideNumber] = useState(0);
+
+    const listRef=useRef();
+
+
+    const handleClick = (direction)=>{
+        setIsMoved(true);
+        let distance=listRef.current.getBoundingClientRect().x - 50;
+
+        if (direction === "left" && slideNumber > 0)
+        {
+            setSlideNumber(slideNumber -1 );
+            listRef.current.style.transform = `translateX(${ distance + 230 }px)`;
+        }
+
+        if (direction === "right" && slideNumber < 5)
+        {
+            setSlideNumber(slideNumber + 1);
+            listRef.current.style.transform = `translateX(${ distance - 230 }px)`;
+        }
+
+        //console.log(distance);
+
+    }
+
+
     return (
         <div className="list">
            <span className="listTitle">Continue to watch</span>
            <div className="wrapper">
-               <ArrowBackIos className="slideArrow left"/>
-               <div className="container">
+               <ArrowBackIos className="slideArrow left" onClick={()=>handleClick("left")} style={{display:!isMoved && "none"}}/>
+               <div className="container" ref={listRef} >
 
                    <ListItem/>
                    <ListItem/>
@@ -20,10 +49,9 @@ export default function List() {
                    <ListItem/>
                    <ListItem/>
                    <ListItem/>
-                   <ListItem/>
-                   <ListItem/>
+                   
                </div>
-               <ArrowForwardIos className="slideArrow right"/>
+               <ArrowForwardIos className="slideArrow right" onClick={()=>handleClick("right")}/>
            </div>
         </div>
     )
