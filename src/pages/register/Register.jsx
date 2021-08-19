be7b199b16df967a343f51a15dae1583cef9f053
bisaksss <bisaksss@gmail.com>
2021-08-19 15:23:40 +0530

    add useref and usestate

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,6 +1,22 @@
+import { useRef, useState } from "react";
 import "./register.scss";
 
 export default function Register() {
+
+    const[email,setEmail]=useState("");
+    const emailRef=useRef();
+
+    const initializeEmail=()=>{
+        setEmail(emailRef.current.value);
+    }
+
+    const[password,setPassword]=useState("");
+    const passwordRef=useRef();
+
+    const initializePassword=()=>{
+        setPassword(passwordRef.current.value);
+    }
+
     return (
         <div className="register">
             <div className="top">
@@ -19,11 +35,20 @@ export default function Register() {
                 <h3>adipisicing elit. Error nemo recusandae fugit itaqu</h3>
 
                 <p>Bisak sampath</p>
+                {!email ? (
+                     <div className="input">
+                        <input type="email" placeholder="Email Address" ref={emailRef}/>
+                        <button className="regiterButton" onClick={initializeEmail}>Get Started</button>
+                    </div>
 
-                <div className="input">
-                    <input type="email" placeholder="Email Address" />
-                    <button className="regiterButton">Get Started</button>
-                </div>
+                ) : (
+
+                    <div className="input">
+                        <input type="password" placeholder="Password" ref={passwordRef}/>
+                        <button className="regiterButton" onClick={initializePassword}>Start</button>
+                    </div>
+                )}
+               
             </div>
             
         </div>
