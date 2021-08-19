09daaee01d8794297c93f63a1933ed8e83426224
bisaksss <bisaksss@gmail.com>
2021-08-19 14:28:28 +0530

    add register age

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router,Route, Switch } from "react-router-dom";
 import "./app.scss";
 import Home from "./pages/home/Home";
 import Watch from "./pages/watch/Watch";
+import Register from "./pages/register/Register";
 function App() {
   return (
 
@@ -16,6 +17,9 @@ function App() {
      
 
         <Route  path = "/watch" component = {Watch} />
+
+
+        <Route  path = "/register" component = {Register} />
 
 
 
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -3,7 +3,26 @@ import "./register.scss";
 export default function Register() {
     return (
         <div className="register">
+            <div className="top">
+                <div className="wrapper">
+                    <a href="#home" className="logo">SANDEEPA</a>
+                    <button className="loginButton">Sign In</button>
+                </div>
+              
+            </div>
+
+            <div className="container">
+                <h1>Lorem ipsum dolor sit amet consectetur, 
+                    adipisicing elit. Error nemo recusandae fugit itaqu
+                </h1>
+
+                <div className="input">
+                    <input type="email" placeholder="Email Address" />
+                    <button className="regiterButton">Get Started</button>
+                </div>
+            </div>
             
         </div>
+
     )
 }
